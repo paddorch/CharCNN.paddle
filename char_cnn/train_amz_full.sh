82f4de84004ad14df8796232b18d94cb5a1aa140
Yichen Xu <yichen.x@outlook.com>
2021-08-11 18:01:14 +0800

    Stage

--- char_cnn/train_amz_full.sh
+++ char_cnn/train_amz_full.sh
@@ -1,6 +1,7 @@
 python train.py \
-  --train_path /hy-tmp/datasets/amazon_review_full_csv/train.csv \
-  --val_path /hy-tmp/datasets/amazon_review_full_csv/test.csv \
+  --train_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/train.csv \
+  --val_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/test.csv \
   --save_folder output/models_amz_full\
   --batch_size 64 \
-  --val_interval 2000
+  --val_interval 2000 \
+  --gpu 3
