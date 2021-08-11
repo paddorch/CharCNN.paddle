712521337e60a9cd10be680ddf9bfa6ef4e18c73
Yichen Xu <yichen.x@outlook.com>
2021-08-12 02:19:51 +0800

    Update scripts

--- eval_yahoo_answers.sh
+++ eval_yahoo_answers.sh
@@ -1,5 +1,5 @@
 python eval.py \
   --test-path='data/yahoo_answers_csv/test.csv' \
-#  --test-path='/home/xuyichen/.paddlenlp/datasets/YahooAnswers/yahoo_answers_csv/test.csv' \
   --model-path='output/models_yahoo_answers/CharCNN_best.pth.tar' \
 #  --device='gpu:3'
+#  --test-path='/home/xuyichen/.paddlenlp/datasets/YahooAnswers/yahoo_answers_csv/test.csv' \
--- train_amz_full.sh
+++ train_amz_full.sh
@@ -1,10 +1,10 @@
 python train.py \
   --train_path data/amazon_review_full_csv/train.csv.split_train \
   --val_path data/amazon_review_full_csv/train.csv.split_val \
-#  --train_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/train.csv \
-#  --val_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/test.csv \
   --save_folder output/models_amz_full \
   --batch_size 64 \
-#  --device 3 \
   --val_interval 2000 \
   --data_augment True
+
+#  --train_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/train.csv \
+#  --val_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/test.csv \
