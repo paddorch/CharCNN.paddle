6fa5149b8062ebc7b8964f6ea03d977dd10f5240
ZubinGou <zebgou@gmail.com>
2021-08-11 16:29:47 +0800

    add split data

--- char_cnn/train_ag_news.sh
+++ char_cnn/train_ag_news.sh
@@ -1,4 +1,4 @@
 python train.py \
-  --train_path data/ag_news_csv/train.csv \
-  --val_path data/ag_news_csv/test.csv \
+  --train_path data/ag_news_csv/train.csv.split_train \
+  --val_path data/ag_news_csv/train.csv.split_val \
   --save_folder output/models_AG_NEWS \
