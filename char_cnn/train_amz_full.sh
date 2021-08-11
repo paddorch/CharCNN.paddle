91314dd81e89a2a8c67cc3f8270be0c4eb71beba
ZubinGou <zebgou@gmail.com>
2021-08-11 18:03:14 +0800

    test bar

--- char_cnn/train_amz_full.sh
+++ char_cnn/train_amz_full.sh
@@ -1,6 +1,6 @@
 python train.py \
-  --train_path /hy-tmp/datasets/amazon_review_full_csv/train.csv \
-  --val_path /hy-tmp/datasets/amazon_review_full_csv/test.csv \
+  --train_path /hy-tmp/datasets/amazon_review_full_csv/train.csv.split_train \
+  --val_path /hy-tmp/datasets/amazon_review_full_csv/train.csv.split_val \
   --save_folder output/models_amz_full\
   --batch_size 64 \
   --val_interval 2000
--- char_cnn/train_yahoo_answers.sh
+++ char_cnn/train_yahoo_answers.sh
@@ -1,6 +1,6 @@
 python train.py \
-  --train_path /hy-tmp/datasets/yahoo_answers_csv/train.csv \
-  --val_path /hy-tmp/datasets/yahoo_answers_csv/test.csv \
+  --train_path /hy-tmp/datasets/yahoo_answers_csv/train.csv.split_train \
+  --val_path /hy-tmp/datasets/yahoo_answers_csv/train.csv.split_dev \
   --save_folder output/models_yahoo_answers\
   --batch_size 64 \
   --val_interval 2000
