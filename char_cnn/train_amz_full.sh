f19f731eebec82b97c608cf984f22b89936f8959
Yichen Xu <yichen.x@outlook.com>
2021-08-11 19:20:48 +0800

    Update

--- char_cnn/train_amz_full.sh
+++ char_cnn/train_amz_full.sh
@@ -1,7 +1,7 @@
 python train.py \
-  --train_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/train.csv \
-  --val_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/test.csv \
-  --save_folder output/models_amz_full\
+  --train_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/train.csv.split_train \
+  --val_path /home/xuyichen/.paddlenlp/datasets/AmazonReviewFull/amazon_review_full_csv/train.csv.split_val \
+  --save_folder output/models_amz_full \
   --batch_size 64 \
   --gpu 3 \
   --val_interval 2000 \
--- char_cnn/train_yahoo_answers.sh
+++ char_cnn/train_yahoo_answers.sh
@@ -1,6 +1,6 @@
 python train.py \
-  --train_path /home/xuyichen/.paddlenlp/datasets/YahooAnswers/yahoo_answers_csv/train.csv \
-  --val_path /home/xuyichen/.paddlenlp/datasets/YahooAnswers/yahoo_answers_csv/test.csv \
+  --train_path /home/xuyichen/.paddlenlp/datasets/YahooAnswers/yahoo_answers_csv/train.csv.split_train \
+  --val_path /home/xuyichen/.paddlenlp/datasets/YahooAnswers/yahoo_answers_csv/train.csv.split_val \
   --save_folder output/models_yahoo_answers \
   --batch_size 64 \
   --gpu 2 \
